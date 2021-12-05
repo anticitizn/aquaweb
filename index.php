c69d83cbbc4bb3702ef6f82c1f58eb55aaac7bb6
dragonXP370-creater <tim.huprich.20@lehre.mosbach.dhbw.de>
2021-12-05 11:31:47 +0100

    header.php

--- index.php
+++ index.php
@@ -9,22 +9,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">AquaWeb</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="navbar-nav">
-                    <a class="nav-link active" href="#">Home</a>
-                    <a class="nav-link" href="aquarium.php">My aquarium</a>
-                    <a class="nav-link" href="statistics.php">Statistics</a>
-                    <a class="nav-link" href="shop.php">Shop</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+<?php include 'php/templates/header.php';?>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
