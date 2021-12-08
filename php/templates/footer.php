fc2632ebd27391dd59861bdd17c1b31e04441ae9
RiedlNico <90173728+RiedlNico@users.noreply.github.com>
2021-12-08 15:28:15 +0100

    Footer überarbeitet

--- php/templates/footer.php
+++ php/templates/footer.php
@@ -1,29 +1,29 @@
 <!-- Footer -->
 <footer class="bg-light text-center text-black">
-    <!-- Grid container -->
+    <!-- Responsive Grid container -->
     <div class="container p-4">
-        <!-- Section: Social media -->
+        <!-- Section for our GitHub-->
         <section class="mb-4">
             <!-- Github -->
             <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/kreug/aquaweb" role="button"
             ><i class="fab fa-github"></i
                 ></a>
         </section>
-        <!-- Section: Social media -->
+        <!-- Section for our GitHub -->
 
-        <!-- Section: Text -->
+        <!-- Section for some text -->
         <section class="mb-4">
             <p>
                 <h>AquaWeb</h>
             </p>
         </section>
-        <!-- Section: Text -->
+        <!-- Section for some text -->
 
-        <!-- Section: Links -->
+        <!-- Section for Links -->
         <section>
-            <!--Grid row-->
+            <!-- Responsive Grid row-->
             <div class="row">
-                <!--Grid column-->
+                <!-- Responsive Grid column-->
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                     <h5 class="text">Links</h5>
 
@@ -36,18 +36,18 @@
                         </li>
                     </ul>
                 </div>
-                <!--Grid column-->
+                <!-- Responsive Grid column-->
             </div>
-            <!--Grid row-->
+            <!-- Responsive Grid row-->
         </section>
-        <!-- Section: Links -->
+        <!-- Section for Links -->
     </div>
-    <!-- Grid container -->
+    <!-- Responsive Grid container -->
 
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2020 Copyright:
-        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        © 2021 Copyright:
+        <span>AquaWeb</span>
     </div>
     <!-- Copyright -->
 </footer>
