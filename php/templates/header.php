06cdf4d1bf2ea379ff630cdf5b7736c163ac1b97
RiedlNico <90173728+RiedlNico@users.noreply.github.com>
2021-12-11 12:21:47 +0100

    Neuer Carousel Header.php

--- php/templates/header.php
+++ php/templates/header.php
@@ -1,18 +1,65 @@
-<header>
-    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">AquaWeb</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="navbar-nav">
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-black bg-light fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">AquaWeb</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item active">
                     <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="php/aquarium.php">My aquarium</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="php/templates/statistics.php">Statistics</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="shop.php">Shop</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!--Header-->
+<header>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" style="background-image: url('https://cdn.pixabay.com/photo/2014/06/27/12/36/fish-378286_960_720.jpg')">
+                <div class="carousel-caption">
+                    <h2>Willkommen zu AquaWeb</h2>
+                    <p>Hier können sie ihre eigenes Online-Aquarium pflegen</p>
+                </div>
+            </div>
+            <div class="carousel-item" style="background-image: url('https://cdn.pixabay.com/photo/2016/11/29/09/43/koi-fish-1868779_960_720.jpg')">
+                <div class="carousel-caption">
+                    <h2>Füttern sie ihre Fische</h2>
+                    <p>Sie können ihre Fische füttern und sie dadurch wachsen lassen</p>
+                </div>
+            </div>
+            <div class="carousel-item" style="background-image: url('https://cdn.pixabay.com/photo/2012/03/03/23/54/animal-21668_960_720.jpg')">
+                <div class="carousel-caption">
+                    <h2>Kaufen sie neue Fische</h2>
+                    <p>Im Shop können sie neue und größere Fische kaufen</p>
                 </div>
             </div>
         </div>
-    </nav>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </header>
+
