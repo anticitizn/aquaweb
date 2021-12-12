d01b7c34ee16a54ae450368bc096911be136cb6f
lloeffler <lukas.loeffler23@gmail.com>
2021-12-12 10:45:48 +0100

    fixed quaweb.sql

--- aquaweb.sql
+++ aquaweb.sql
@@ -4,5 +4,5 @@ CREATE TABLE `users` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(50) NOT NULL ,
   `password` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`id`), UNIQUE (`email`)
+  PRIMARY KEY (`id`), UNIQUE (`username`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
