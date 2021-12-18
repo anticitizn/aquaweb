7cb45e9a1b844eed03e1b0fe27bf29d5562607c8
dragonXP370-creater <tim.huprich.20@lehre.mosbach.dhbw.de>
2021-12-18 13:16:23 +0100

    added comments in my files

--- php/database/connection.php
+++ php/database/connection.php
@@ -3,38 +3,21 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Zum Aufbau der Verbindung zur Datenbank
+// connection to database
 
-// Zum Aufbau der Verbindung zur Datenbank
-// die Daten erhalten Sie von Ihrem Provider
+// we created a local database on the ip-address '51.15.100.196'
+// thats because we had trouble importing the php-Myadmin database
 define ( 'MYSQL_HOST',      '51.15.100.196' );
 
-// bei XAMPP ist der MYSQL_Benutzer: root
+// username = aquaweb
+//password = webaqua123
 define ( 'MYSQL_BENUTZER',  'aquaweb' );
 define ( 'MYSQL_KENNWORT',  'webaqua123' );
 
-//Unsere Datenbank aquaweb
+//the database aquaweb
 define ( 'MYSQL_DATENBANK', 'aquaweb' );
 
+// variable with all sql-connection paramters for use on different sides
 $db_connect= mysqli_connect (MYSQL_HOST, MYSQL_BENUTZER,  MYSQL_KENNWORT,  MYSQL_DATENBANK);
-
-/*if ( $db_connect)
-{
-    echo 'Verbindung erfolgreich: '."<br>";
-    
-
-    $abfrage = "SELECT * FROM fish";
-
-    $result = mysqli_query($db_connect,$abfrage);
-
-    while($row = mysqli_fetch_assoc($result)){
-    echo $row["id"] . ", " .
-        $row["name"] . ", " .
-        $row["price"] . " <br> " .
-        "";
-    }
-  
-}*/
-
 
 ?>
