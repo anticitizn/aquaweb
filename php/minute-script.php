430854e31fdef65a767278b6a45bd0014992eea9
danny <metheorita@gmail.com>
2021-12-19 03:27:08 +0100

    Update refresh script

--- php/minute-script.php
+++ php/minute-script.php
@@ -2,7 +2,10 @@
 include('database/connection.php');
 $visitor_query = "DELETE FROM users_visitors WHERE last_visit < (NOW() - INTERVAL 1 HOUR)";
 $visitor_result = mysqli_query($db_connect, $visitor_query);
+echo $visitor_result;
 
 $dead_fish_query = "DELETE FROM users_fish WHERE lastFed < (NOW() - INTERVAL 3 DAY)";
 $dead_fish_result = mysqli_query($db_connect, $dead_fish_query);
+
+echo $dead_fish_result;
 ?>
