0f9bdbeb384276daafd56d544d6d77427b8f6311
danny <metheorita@gmail.com>
2021-12-20 11:52:48 +0100

    Manually add ipv6 function

--- php/aquarium.php
+++ php/aquarium.php
@@ -1,6 +1,28 @@
 <?php
 session_start();
 $indexphp = '';
+
+function ip2long_v6($ip) {
+    $ip_n = inet_pton($ip);
+    $bin = '';
+    for ($bit = strlen($ip_n) - 1; $bit >= 0; $bit--) {
+        $bin = sprintf('%08b', ord($ip_n[$bit])) . $bin;
+    }
+
+    if (function_exists('gmp_init')) {
+        return gmp_strval(gmp_init($bin, 2), 10);
+    } elseif (function_exists('bcadd')) {
+        $dec = '0';
+        for ($i = 0; $i < strlen($bin); $i++) {
+            $dec = bcmul($dec, '2', 0);
+            $dec = bcadd($dec, $bin[$i], 0);
+        }
+        return $dec;
+    } else {
+        trigger_error('GMP or BCMATH extension not installed!', E_USER_ERROR);
+    }
+}
+
 // log ip 
 $ip = $_SERVER['REMOTE_ADDR'];
 
